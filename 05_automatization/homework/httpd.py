6b32c46e365bc7a234b0ac649ba86a56d055e8ad
Sergey Chernyak <chernyaksergey@gmail.com>
2018-09-19 20:28:14 +0300

    start

--- 05_automatization/homework/httpd.py
+++ 05_automatization/homework/httpd.py
@@ -1,19 +1,27 @@
 import asyncio
 
-
-async def tcp_echo_client(message):
-    reader, writer = await asyncio.open_connection(
-        '127.0.0.1', 8888)
-
-    print(f'Send: {message!r}')
-    writer.write(message.encode())
-
+async def handle_echo(reader, writer):
     data = await reader.read(100)
-    print(f'Received: {data.decode()!r}')
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
 
-    print('Close the connection')
+    print(f"Received {message!r} from {addr!r}")
+
+    print(f"Send: {message!r}")
+    writer.write(data)
+    await writer.drain()
+
+    print("Close the connection")
     writer.close()
-    await writer.wait_closed()
 
+async def main():
+    server = await asyncio.start_server(
+        handle_echo, '127.0.0.1', 8080)
 
-asyncio.run(tcp_echo_client('Hello World!'))
+    addr = server.sockets[0].getsockname()
+    print(f'Serving on {addr}')
+
+    async with server:
+        await server.serve_forever()
+
+asyncio.run(main())
